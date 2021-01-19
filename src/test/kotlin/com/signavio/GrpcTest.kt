6f7f3a5bae5146e33bd2ab58f6ed4f0d55cdf513
Victor Fonseca <victorfonseca>
2021-01-19 06:58:47 +0000

    Initial commit

---
+++ src/test/kotlin/com/signavio/GrpcTest.kt
@@ -0,0 +1,19 @@
+package com.signavio
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import javax.inject.Inject
+
+@MicronautTest
+class GrpcTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
